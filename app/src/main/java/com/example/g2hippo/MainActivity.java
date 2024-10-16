1f2ed8fc4ea4ab0cc2c68624a2f89a17681d434a
Samsung120 <Samsung@sch120.ru>
2024-10-16 22:35:12 +0600

    Version 1.0

---
+++ app/src/main/java/com/example/g2hippo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.g2hippo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    int n = 0;
+    TextView textCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        textCount = findViewById(R.id.textView3);
+    }
+
+    public void tapHippo(View view) {
+        n += 1;
+        textCount.setText("Потапано " + n + " раз");
+    }
+}
